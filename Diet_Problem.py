4abbbe259aa5d13c88be740ea06ebc5b38554974
jtuckman9@gmail.com <Jtuckman9@gmail.com>
2020-05-17 18:31:47 -0400

    Comments

--- Diet_Problem.py
+++ Diet_Problem.py
@@ -8,40 +8,49 @@ import random
 
 # We want to find the most enjoyment while not crossing our restraints
 
-
+# Dictionaries holding details of each treat
 Brownie = {"Name": "Brownie", "Sugar": 3, "Fat": 4, "Enjoyment": 6}
 Cake = {"Name": "Cake", "Sugar": 4, "Fat": 5, "Enjoyment": 6}
+
+# List of possible treats to be randomized
 food = [Brownie, Cake]
 
-
+# Max conditions to be our checks
 MAXSUG = 12
 MAXFAT = 20
 MAXENJOYMENT = 0
 
+# Counts to be compared to our conditional checks
 countSUG = 0
 countFAT = 0
 countEnjoyment = 0
 
+# Temporary holder list and final list
 final = []
 new = []
+
+# Kind of lazy, but running 100 times as the correct answer will come in that time with randomization
 NumRuns = 0
 
 
 while NumRuns < 100:
     NumRuns +=1
+    # Checking that we have not gone over our conditions
     while countSUG <= MAXSUG and countFAT <= MAXFAT:
+        # Taking one of our foods randomly
         randomfood = food[random.randint(0, 1)]
         countSUG += randomfood["Sugar"]
         countFAT += randomfood["Fat"]
         countEnjoyment += randomfood["Enjoyment"]
 
-        #reset for next run
+        #reset for next run if broken
         if countSUG > MAXSUG or countFAT > MAXFAT:
             countSUG = 0
             countFAT = 0
             countEnjoyment = 0
             new = []
             break
+        # otherwise we are checking if this is a new best
         else:
             new.append(randomfood.copy())
             if countEnjoyment >= MAXENJOYMENT:
